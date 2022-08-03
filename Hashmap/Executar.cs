99ec5f9025cf9b9054d3ed9b9bb2879e15bddfbd
everton david <everton.097@gmail.com>
2022-08-03 10:37:59 -0300

    Initial commit

---
+++ Hashmap/Executar.cs
@@ -0,0 +1,16 @@
+﻿namespace Hashmap;
+
+public class Executar
+{
+    static void Main(string[] args)
+    {
+        IDictionary<int, string> flowerNames = new Dictionary<int, string>();
+
+        flowerNames.Add(1, "Rose");
+        flowerNames.Add(2, "Jasmine");
+        flowerNames.Add(3, "Lili");
+
+        foreach (KeyValuePair<int, string> kvp in flowerNames)
+            Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+    }
+}
